e8bc7e7cd186fc4cd2af1ee5d12659390d7f6b8d
Suraj Maity <surajmaity981@gmail.com>
2024-04-20 14:14:54 +0530

    Upto 19th April

--- src/main/java/com/cts/cms/controller/CommonController.java
+++ src/main/java/com/cts/cms/controller/CommonController.java
@@ -6,16 +6,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cts.cms.dto.AdminDto;
 import com.cts.cms.dto.ClaimDto;
 import com.cts.cms.dto.MemberDto;
+import com.cts.cms.entity.Admin;
 import com.cts.cms.entity.Claim;
+import com.cts.cms.entity.Member;
+import com.cts.cms.repository.AdminRepository;
 import com.cts.cms.repository.ClaimRepository;
+import com.cts.cms.repository.MemberRepository;
 import com.cts.cms.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
@@ -26,10 +33,12 @@ public class CommonController {
     private ClaimRepository claimRepository;
 
     @Autowired
-    MemberService memberService;
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private MemberService memberService;
 
     List<Long> memberIdList = null;
-
 
     @GetMapping("/")
     public String home() {
@@ -41,8 +50,79 @@ public class CommonController {
         return "login";
     }
 
+    @GetMapping("/dashboard")
+    public String dashboard(HttpSession session) {
+
+        if(session.getAttribute("username") == null || session.getAttribute("username").equals("")) {
+            return "redirect:/login";
+        }
+
+        return "dashboard";
+    }
+
+	/*
+	@PostMapping("/registerAdmin")
+	public String adminRegistration(@ModelAttribute AdminDto adminDto) {
+
+
+		Admin admin = new Admin();
+
+		admin.setUsername(adminDto.getUsername());
+		admin.setPassword(adminDto.getPassword());
+
+		adminRepository.save(admin);
+		return "redirect:/login";
+	}
+	*/
+
+
+    @PostMapping("/checkLogIn")
+    public String checkLogIn(@ModelAttribute AdminDto adminDto, HttpSession session, ModelMap modelMap) {
+        try {
+            String username = adminDto.getUsername();
+            if(adminRepository
+                    .existsByUsernameAndPassword(username, adminDto.getPassword())) {
+
+                session.setAttribute("username", username);
+                return "redirect:/dashboard";
+            }
+            else {
+                modelMap.put("error", "Invalid account");
+                return "redirect:/login";
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/register")
+    public String registerMember(HttpSession session) {
+
+        if(session.getAttribute("username") == null || session.getAttribute("username").equals("")) {
+            return "redirect:/login";
+        }
+
+        return "member_registration";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("username");
+        session.invalidate();
+        return "redirect:/";
+    }
+
+
     @GetMapping("/claimrequest")
-    public String claimRequest(Model model) {
+    public String claimRequest(Model model, HttpSession session) {
+
+
+        if(session.getAttribute("username") == null || session.getAttribute("username").equals("")) {
+            return "redirect:/login";
+        }
+
         List<String> stringMemberIdLists = null;
 
         memberIdList = memberService.getAllMemberId();
--- src/main/java/com/cts/cms/entity/Admin.java
+++ src/main/java/com/cts/cms/entity/Admin.java
@@ -31,4 +31,28 @@ public class Admin {
     @Column(name = "password", nullable = false)
     private String password;
 
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
